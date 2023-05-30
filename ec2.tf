8ddf0326169cbe7bb93396eedab224238d9db949
anddycabrera <anddycabrera@gmail.com>
2023-05-30 11:16:04 -0400

    update ec2

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,48 @@
+resource "aws_security_group" "allow_traffic" {
+  name        = "allow_http_https"
+  description = "Allow inbound traffic from Internet on HTTP and HTTPS"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "app" {
-  ami           = "ami-0c94855ba95c574c8" # an Amazon Linux 2 AMI
+  ami           = "ami-01107263728f3bef4" # an Amazon Linux 2 AMI
   instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.allow_traffic.id]
+
+  key_name = "nlp-key" # replace with the name of your existing key pair
+
+  root_block_device {
+    volume_type = "gp3"
+    volume_size = 8 # change this to the size you want in GB
+  }
 
   tags = {
     Name = "my-app"
