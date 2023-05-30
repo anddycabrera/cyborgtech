b27847a3d489af638bbe2f7125a705bb291b45d3
anddycabrera <anddycabrera@gmail.com>
2023-05-29 23:44:27 -0400

    update repo

--- ec2.tf
+++ ec2.tf
@@ -14,8 +14,8 @@ resource "aws_instance" "app" {
               curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
               chmod +x /usr/local/bin/docker-compose
               systemctl start docker
-              git clone https://github.com/anddycabrera/starlogs.git
-              cd starlogs-main
+              git clone https://github.com/anddycabrera/cyborgtech.git
+              cd cyborgtech-main
               docker-compose up -d
               EOF
 }
