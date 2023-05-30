9b136eef0ee1804d778a45db5f6bbab4ca47fe53
anddycabrera <anddycabrera@gmail.com>
2023-05-30 15:52:06 -0400

    destroy

--- route53.tf
+++ route53.tf
@@ -2,10 +2,10 @@ data "aws_route53_zone" "this" {
     name = "mystarlog.com"
 }
 
-resource "aws_route53_record" "example" {
-  zone_id = data.aws_route53_zone.this.zone_id
-  name    = ""
-  type    = "A"
-  ttl     = "300"
-  records = [aws_instance.app.public_ip]
-}
+# resource "aws_route53_record" "example" {
+#   zone_id = data.aws_route53_zone.this.zone_id
+#   name    = ""
+#   type    = "A"
+#   ttl     = "300"
+#   records = [aws_instance.app.public_ip]
+# }
