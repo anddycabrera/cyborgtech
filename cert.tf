e87af2a4fe13a5c756a6b4f986babd82ea6cce7c
anddycabrera <anddycabrera@gmail.com>
2023-05-31 11:41:26 -0400

    destroy

--- cert.tf
+++ cert.tf
@@ -1,17 +1,17 @@
-module "acm" {
-  source  = "terraform-aws-modules/acm/aws"
-  version = "~> 4.0"
+# module "acm" {
+#   source  = "terraform-aws-modules/acm/aws"
+#   version = "~> 4.0"
 
-  domain_name  = "mystarlog.com"
-  zone_id      = data.aws_route53_zone.this.zone_id
+#   domain_name  = "mystarlog.com"
+#   zone_id      = data.aws_route53_zone.this.zone_id
 
-  subject_alternative_names = [
-    "*.mystarlog.com",
-  ]
+#   subject_alternative_names = [
+#     "*.mystarlog.com",
+#   ]
 
-  wait_for_validation = false
+#   wait_for_validation = false
 
-  tags = {
-    Name = "mystarlog.com"
-  }
-}
+#   tags = {
+#     Name = "mystarlog.com"
+#   }
+# }
