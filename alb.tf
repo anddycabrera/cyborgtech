ad23a1864d57385d5493a5185fcbf0211cdc7d9e
anddycabrera <anddycabrera@gmail.com>
2023-05-30 22:55:30 -0400

    update

--- alb.tf
+++ alb.tf
@@ -28,8 +28,36 @@ resource "aws_lb" "lb" {
   enable_deletion_protection = true
 }
 
-resource "aws_lb_target_group" "tg_8080" {
-  name     = "tf-example-lb-tg-8080"
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.cert.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
+resource "aws_lb_target_group" "tg" {
+  name     = "tf-example-lb-tg"
   port     = 8080
   protocol = "HTTP"
   vpc_id   = data.aws_vpc.default.id
@@ -43,58 +71,15 @@ resource "aws_lb_target_group" "tg_8080" {
   }
 }
 
-resource "aws_lb_target_group" "tg_8000" {
-  name     = "tf-example-lb-tg-8000"
-  port     = 8000
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default.id
-
-  health_check {
-    interval            = 30
-    path                = "/api/v1/"
-    timeout             = 3
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-}
-
-resource "aws_lb_listener" "listener_8080" {
-  load_balancer_arn = aws_lb.lb.arn
-  port              = 8080
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.tg_8080.arn
-  }
-}
-
-resource "aws_lb_listener" "listener_8000" {
-  load_balancer_arn = aws_lb.lb.arn
-  port              = 8000
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.tg_8000.arn
-  }
-}
-
-resource "aws_lb_target_group_attachment" "tga_8080" {
-  target_group_arn = aws_lb_target_group.tg_8080.arn
+resource "aws_lb_target_group_attachment" "tga" {
+  target_group_arn = aws_lb_target_group.tg.arn
   target_id        = aws_instance.app.id
   port             = 8080
 }
 
-resource "aws_lb_target_group_attachment" "tga_8000" {
-  target_group_arn = aws_lb_target_group.tg_8000.arn
-  target_id        = aws_instance.app.id
-  port             = 8000
-}
-
 resource "aws_security_group" "elb_sg" {
   name        = "elb_sg"
-  description = "Allow inbound traffic on HTTP and HTTPS"
+  description = "Allow inbound traffic on HTTP, HTTPS, and port 8000"
 
   ingress {
     from_port   = 80
@@ -111,13 +96,6 @@ resource "aws_security_group" "elb_sg" {
   }
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
     from_port   = 8000
     to_port     = 8000
     protocol    = "tcp"
@@ -129,5 +107,92 @@ resource "aws_security_group" "elb_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "api_v1_tg" {
+  name     = "tf-example-api-v1-tg"
+  port     = 8000
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    interval            = 30
+    path                = "/api/v1/"
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_target_group" "docs_tg" {
+  name     = "tf-example-docs-tg"
+  port     = 8000
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    interval            = 30
+    path                = "/docs"
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_target_group_attachment" "api_v1_tga" {
+  target_group_arn = aws_lb_target_group.api_v1_tg.arn
+  target_id        = aws_instance.app.id
+  port             = 8000
+}
+
+resource "aws_lb_target_group_attachment" "docs_tga" {
+  target_group_arn = aws_lb_target_group.docs_tg.arn
+  target_id        = aws_instance.app.id
+  port             = 8000
+}
+
+resource "aws_lb_listener" "http_8000" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = "8000"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "The default response for port 8000"
+      status_code  = "200"
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "api_v1_listener_rule" {
+  listener_arn = aws_lb_listener.http_8000.arn
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.api_v1_tg.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/api/v1/*"]
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "docs_listener_rule" {
+  listener_arn = aws_lb_listener.http_8000.arn
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.docs_tg.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/docs/*"]
+    }
   }
 }
