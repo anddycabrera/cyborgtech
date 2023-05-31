06be61d8d0ed8d65dcea3be2e8c2b07c61cbaf67
anddycabrera <anddycabrera@gmail.com>
2023-05-31 12:15:01 -0400

    security group

--- alb.tf
+++ alb.tf
@@ -131,6 +131,20 @@ resource "aws_security_group" "elb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
