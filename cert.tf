28cb5f0b998f9d5e8955baeabd698ac1b7ba1d23
anddycabrera <anddycabrera@gmail.com>
2023-05-30 23:08:13 -0400

    destroy

--- cert.tf
+++ cert.tf
@@ -1,24 +1,26 @@
-resource "aws_acm_certificate" "cert" {
-  domain_name       = "*.mystarlog.com"
-  validation_method = "DNS"
+# resource "aws_acm_certificate" "cert" {
+#   domain_name       = "mystarlog.com"
+#   validation_method = "DNS"
 
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+#   subject_alternative_names = ["*.mystarlog.com"]
 
-resource "aws_route53_record" "cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
 
-  zone_id = data.aws_route53_zone.this.zone_id
-  name    = each.value.name
-  type    = each.value.type
-  records = [each.value.record]
-  ttl     = 60
-}
+# resource "aws_route53_record" "cert_validation" {
+#   for_each = {
+#     for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+#       name   = dvo.resource_record_name
+#       record = dvo.resource_record_value
+#       type   = dvo.resource_record_type
+#     }
+#   }
+
+#   zone_id = data.aws_route53_zone.this.zone_id
+#   name    = each.value.name
+#   type    = each.value.type
+#   records = [each.value.record]
+#   ttl     = 60
+# }
