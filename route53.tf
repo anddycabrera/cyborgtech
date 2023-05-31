420991c4918950384953e4a2d986879b445e2557
anddycabrera <anddycabrera@gmail.com>
2023-05-30 23:14:22 -0400

    recreate all

--- route53.tf
+++ route53.tf
@@ -1,15 +1,15 @@
-# data "aws_route53_zone" "this" {
-#     name = "mystarlog.com"
-# }
+data "aws_route53_zone" "this" {
+    name = "mystarlog.com"
+}
 
-# resource "aws_route53_record" "mystarlog" {
-#   zone_id = data.aws_route53_zone.this.zone_id
-#   name    = ""
-#   type    = "A"
+resource "aws_route53_record" "mystarlog" {
+  zone_id = data.aws_route53_zone.this.zone_id
+  name    = ""
+  type    = "A"
 
-#   alias {
-#     name                   = aws_lb.lb.dns_name
-#     zone_id                = aws_lb.lb.zone_id
-#     evaluate_target_health = false
-#   }
-# }
+  alias {
+    name                   = aws_lb.lb.dns_name
+    zone_id                = aws_lb.lb.zone_id
+    evaluate_target_health = false
+  }
+}
