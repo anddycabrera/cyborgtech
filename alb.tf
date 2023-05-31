5956c50ea244834e1e0cf6696b5a36d20d8cdfec
anddycabrera <anddycabrera@gmail.com>
2023-05-30 21:55:29 -0400

    update

---
+++ alb.tf
@@ -0,0 +1,102 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-east-2a"
+
+  tags = {
+    Name = "Default subnet for us-west-2a"
+  }
+}
+
+resource "aws_lb" "lb" {
+  name               = "tf-nlp-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.elb_sg.id]
+  subnets            = [aws_default_subnet.default_az1.id]
+
+  enable_deletion_protection = true
+
+  access_logs {
+    bucket  = "my-access-logs-bucket"
+    prefix  = "lb"
+    enabled = true
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.cert.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
+resource "aws_lb_target_group" "tg" {
+  name     = "tf-example-lb-tg"
+  port     = 8000
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    interval            = 30
+    path                = "/"
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_target_group_attachment" "tga" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.app.id
+  port             = 8000
+}
+
+resource "aws_security_group" "elb_sg" {
+  name        = "elb_sg"
+  description = "Allow inbound traffic on HTTP and HTTPS"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
--- route53.tf
+++ route53.tf
@@ -2,10 +2,14 @@ data "aws_route53_zone" "this" {
     name = "mystarlog.com"
 }
 
-# resource "aws_route53_record" "example" {
-#   zone_id = data.aws_route53_zone.this.zone_id
-#   name    = ""
-#   type    = "A"
-#   ttl     = "300"
-#   records = [aws_instance.app.public_ip]
-# }
+resource "aws_route53_record" "mystarlog" {
+  zone_id = data.aws_route53_zone.this.zone_id
+  name    = ""
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.lb.dns_name
+    zone_id                = aws_lb.lb.zone_id
+    evaluate_target_health = false
+  }
+}
