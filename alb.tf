580675c92d1f93a6ecfc08214a87843e714b5411
anddycabrera <anddycabrera@gmail.com>
2023-05-30 22:41:40 -0400

    update

--- alb.tf
+++ alb.tf
@@ -26,40 +26,11 @@ resource "aws_lb" "lb" {
   subnets            = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
 
   enable_deletion_protection = true
-
 }
 
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.lb.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "redirect"
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-}
-
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = aws_lb.lb.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = aws_acm_certificate.cert.arn
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.tg.arn
-  }
-}
-
-resource "aws_lb_target_group" "tg" {
-  name     = "tf-example-lb-tg"
-  port     = 8000
+resource "aws_lb_target_group" "tg_8080" {
+  name     = "tf-example-lb-tg-8080"
+  port     = 8080
   protocol = "HTTP"
   vpc_id   = data.aws_vpc.default.id
 
@@ -72,8 +43,51 @@ resource "aws_lb_target_group" "tg" {
   }
 }
 
-resource "aws_lb_target_group_attachment" "tga" {
-  target_group_arn = aws_lb_target_group.tg.arn
+resource "aws_lb_target_group" "tg_8000" {
+  name     = "tf-example-lb-tg-8000"
+  port     = 8000
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    interval            = 30
+    path                = "/api/v1/"
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "listener_8080" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = 8080
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg_8080.arn
+  }
+}
+
+resource "aws_lb_listener" "listener_8000" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = 8000
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg_8000.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "tga_8080" {
+  target_group_arn = aws_lb_target_group.tg_8080.arn
+  target_id        = aws_instance.app.id
+  port             = 8080
+}
+
+resource "aws_lb_target_group_attachment" "tga_8000" {
+  target_group_arn = aws_lb_target_group.tg_8000.arn
   target_id        = aws_instance.app.id
   port             = 8000
 }
@@ -92,6 +106,20 @@ resource "aws_security_group" "elb_sg" {
   ingress {
     from_port   = 443
     to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
