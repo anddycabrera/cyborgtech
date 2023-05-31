50cb178580873b8140d509228c06582f75a51e95
anddycabrera <anddycabrera@gmail.com>
2023-05-31 10:02:24 -0400

    alb upgrade

--- alb.tf
+++ alb.tf
@@ -110,48 +110,6 @@ resource "aws_security_group" "elb_sg" {
   }
 }
 
-resource "aws_lb_target_group" "api_v1_tg" {
-  name     = "tf-example-api-v1-tg"
-  port     = 8000
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default.id
-
-  health_check {
-    interval            = 30
-    path                = "/api/v1/"
-    timeout             = 3
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-}
-
-resource "aws_lb_target_group" "docs_tg" {
-  name     = "tf-example-docs-tg"
-  port     = 8000
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default.id
-
-  health_check {
-    interval            = 30
-    path                = "/docs"
-    timeout             = 3
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-}
-
-resource "aws_lb_target_group_attachment" "api_v1_tga" {
-  target_group_arn = aws_lb_target_group.api_v1_tg.arn
-  target_id        = aws_instance.app.id
-  port             = 8000
-}
-
-resource "aws_lb_target_group_attachment" "docs_tga" {
-  target_group_arn = aws_lb_target_group.docs_tg.arn
-  target_id        = aws_instance.app.id
-  port             = 8000
-}
-
 resource "aws_lb_listener" "http_8000" {
   load_balancer_arn = aws_lb.lb.arn
   port              = "8000"
