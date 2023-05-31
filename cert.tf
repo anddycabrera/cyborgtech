566dece1d127a27c2705552749a174b4511db5c5
anddycabrera <anddycabrera@gmail.com>
2023-05-31 00:06:06 -0400

    update

--- cert.tf
+++ cert.tf
@@ -1,16 +1,16 @@
-# resource "aws_acm_certificate" "cert" {
-#   domain_name       = "mystarlog.com"
-#   validation_method = "DNS"
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "mystarlog.com"
+  validation_method = "DNS"
 
-#   subject_alternative_names = ["*.mystarlog.com"]
-# }
+  subject_alternative_names = ["*.mystarlog.com"]
+}
 
-# resource "aws_route53_record" "cert_validation" {
-#   count = length(aws_acm_certificate.cert.domain_validation_options)
+resource "aws_route53_record" "cert_validation" {
+  count = length(aws_acm_certificate.cert.domain_validation_options)
 
-#   zone_id = data.aws_route53_zone.this.zone_id
-#   name    = aws_acm_certificate.cert.domain_validation_options[count.index].resource_record_name
-#   type    = aws_acm_certificate.cert.domain_validation_options[count.index].resource_record_type
-#   records = [aws_acm_certificate.cert.domain_validation_options[count.index].resource_record_value]
-#   ttl     = 60
-# }
+  zone_id = data.aws_route53_zone.this.zone_id
+  name    = aws_acm_certificate.cert.domain_validation_options[count.index].resource_record_name
+  type    = aws_acm_certificate.cert.domain_validation_options[count.index].resource_record_type
+  records = [aws_acm_certificate.cert.domain_validation_options[count.index].resource_record_value]
+  ttl     = 60
+}
